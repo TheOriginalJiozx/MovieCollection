59e3f4ca2756634faa9a3a77900096d5c4045b62
TheOriginalJiozx <omal0001@stud.kea.dk>
2024-04-03 11:45:57 +0200

    User story 7 og 8

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.InputMismatchException;
 
 public class MovieCollection {
     static List<Movie> movies;
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,4 +1,3 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 public class UserInterface {
     private Scanner scanner = new Scanner(System.in);

fc893c156c5b1c9a1b8c955219af02ca6e0e71a0
TheOriginalJiozx <omal0001@stud.kea.dk>
2024-04-03 11:24:43 +0200

    User story 7 og 8

--- src/Filmsamling.java
+++ src/Filmsamling.java
@@ -1,6 +1,3 @@
-import java.util.Scanner;
-import java.util.InputMismatchException;
-
 public class Filmsamling {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
